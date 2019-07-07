f1026ee89588379e5d669d1138f2217608cdfa5f
jeajo <jeajo@LAPTOP-DHLOM4TM>
2019-07-07 11:43:42 +0900

    모델링

---
+++ doc/library.sql
@@ -0,0 +1,65 @@
+-- 도서
+CREATE TABLE `BOOK` (
+	`book_no`     INTEGER     NOT NULL COMMENT '도서번호', -- 도서번호
+	`book_name`   VARCHAR(50) NOT NULL COMMENT '책이름', -- 책이름
+	`book_author` VARCHAR(50) NOT NULL COMMENT '작가이름', -- 작가이름
+	`book_price`  INTEGER     NOT NULL COMMENT '가격' -- 가격
+)
+COMMENT '도서';
+
+-- 도서
+ALTER TABLE `BOOK`
+	ADD CONSTRAINT `PK_BOOK` -- 도서 기본키
+		PRIMARY KEY (
+			`book_no` -- 도서번호
+		);
+
+-- 읽은도서
+CREATE TABLE `MEB_RBOOK` (
+	`meb_no`  INTEGER NOT NULL COMMENT '회원번호', -- 회원번호
+	`book_no` INTEGER NOT NULL COMMENT '도서번호' -- 도서번호
+)
+COMMENT '읽은도서';
+
+-- 읽은도서
+ALTER TABLE `MEB_RBOOK`
+	ADD CONSTRAINT `PK_MEB_RBOOK` -- 읽은도서 기본키
+		PRIMARY KEY (
+			`meb_no`,  -- 회원번호
+			`book_no`  -- 도서번호
+		);
+
+-- 회원
+CREATE TABLE `MEB` (
+	`meb_no` INTEGER     NOT NULL COMMENT '회원번호', -- 회원번호
+	`meb_id` VARCHAR(20) NOT NULL COMMENT '아이디', -- 아이디
+	`meb_pw` VARCHAR(30) NOT NULL COMMENT '비밀번호' -- 비밀번호
+)
+COMMENT '회원';
+
+-- 회원
+ALTER TABLE `MEB`
+	ADD CONSTRAINT `PK_MEB` -- 회원 기본키
+		PRIMARY KEY (
+			`meb_no` -- 회원번호
+		);
+
+-- 읽은도서
+ALTER TABLE `MEB_RBOOK`
+	ADD CONSTRAINT `FK_BOOK_TO_MEB_RBOOK` -- 도서 -> 읽은도서
+		FOREIGN KEY (
+			`book_no` -- 도서번호
+		)
+		REFERENCES `BOOK` ( -- 도서
+			`book_no` -- 도서번호
+		);
+
+-- 읽은도서
+ALTER TABLE `MEB_RBOOK`
+	ADD CONSTRAINT `FK_MEB_TO_MEB_RBOOK` -- 회원 -> 읽은도서
+		FOREIGN KEY (
+			`meb_no` -- 회원번호
+		)
+		REFERENCES `MEB` ( -- 회원
+			`meb_no` -- 회원번호
+		);
